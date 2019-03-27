82878f5528ce3d2441c80698b0419291c072e853
maggievu <maggievu91@gmail.com>
2019-03-27 02:22:25 -0700

    production build v3

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 const path = require('path')
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
 	// Serve any static files
 	app.use(express.static(path.join(__dirname, 'client/build')));
 	// Handle React routing, return all requests to React app
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
-}
+// }
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
