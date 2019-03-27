b5f040a97af198596d9f4bcd157a194998243129
maggievu <maggievu91@gmail.com>
2019-03-27 01:49:25 -0700

    create a production build

---
+++ client/build/precache-manifest.ba54e1aa75c2c6261baee147adf77aa0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0e6e2abea88cd11ab7186bb2376611e",
+    "url": "/static/media/android.a0e6e2ab.png"
+  },
+  {
+    "revision": "6b52e05b415c0dea1554",
+    "url": "/static/css/main.3611c8f0.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d5ebf8d25cea592837d7",
+    "url": "/static/js/2.d5ebf8d2.chunk.js"
+  },
+  {
+    "revision": "a27e2080b1bd4f351d877c9b304a699d",
+    "url": "/static/media/phone-screen.a27e2080.png"
+  },
+  {
+    "revision": "5bf0929148807039915fa9f8ce5edf8d",
+    "url": "/static/media/hand-holding.5bf09291.png"
+  },
+  {
+    "revision": "8459d1550c2e43ff84b9542256619927",
+    "url": "/static/media/intro-bg.8459d155.jpeg"
+  },
+  {
+    "revision": "b7c2439c3850b26fb98e626200d95a60",
+    "url": "/static/media/ios.b7c2439c.png"
+  },
+  {
+    "revision": "6b52e05b415c0dea1554",
+    "url": "/static/js/main.6b52e05b.chunk.js"
+  },
+  {
+    "revision": "e21119b2455849f7ceab8feec47c373c",
+    "url": "/static/media/about-bg2.e21119b2.jpg"
+  },
+  {
+    "revision": "5ae428f2ecac24d9328c3bf4772ebd02",
+    "url": "/static/media/team.5ae428f2.jpeg"
+  },
+  {
+    "revision": "43039c83afc6c034500e314af5bc971b",
+    "url": "/static/media/maggie.43039c83.jpg"
+  },
+  {
+    "revision": "ebd9a8a11a85d305e68d1a306f0afb26",
+    "url": "/static/media/ta.ebd9a8a1.jpg"
+  },
+  {
+    "revision": "c92c79b438882b7ad5564d98693fe30b",
+    "url": "/static/media/diego.c92c79b4.jpg"
+  },
+  {
+    "revision": "ef4aa3286d3a86d6797d6979c0088c56",
+    "url": "/static/media/julia.ef4aa328.jpg"
+  },
+  {
+    "revision": "7c6d22498663aed3136aea5f27b8830f",
+    "url": "/static/media/andra.7c6d2249.jpg"
+  },
+  {
+    "revision": "6a1b7ce578ddfcbc80e9a48a022b0c84",
+    "url": "/index.html"
+  }
+];
