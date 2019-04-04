3139c4f021927c754705cb063c1d42c98148976d
maggievu <maggievu91@gmail.com>
2019-04-04 00:02:04 -0700

    build v3

---
+++ client/build/precache-manifest.505d017d2771962f40f12b9f55e31674.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "541f161e297557d916ca9b9681ca1a9b",
+    "url": "/static/media/search.541f161e.png"
+  },
+  {
+    "revision": "79091169ed1a6ec177ef",
+    "url": "/static/css/main.c6e8d721.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "48df04c8c892380e13d7",
+    "url": "/static/js/2.80464700.chunk.js"
+  },
+  {
+    "revision": "0ee035ae8bbadb8936ecf28c11452682",
+    "url": "/static/media/portfolio.0ee035ae.png"
+  },
+  {
+    "revision": "5bf0929148807039915fa9f8ce5edf8d",
+    "url": "/static/media/hand-holding.5bf09291.png"
+  },
+  {
+    "revision": "8459d1550c2e43ff84b9542256619927",
+    "url": "/static/media/intro-bg.8459d155.jpeg"
+  },
+  {
+    "revision": "b7c2439c3850b26fb98e626200d95a60",
+    "url": "/static/media/ios.b7c2439c.png"
+  },
+  {
+    "revision": "a0e6e2abea88cd11ab7186bb2376611e",
+    "url": "/static/media/android.a0e6e2ab.png"
+  },
+  {
+    "revision": "79091169ed1a6ec177ef",
+    "url": "/static/js/main.1ad2e43d.chunk.js"
+  },
+  {
+    "revision": "e21119b2455849f7ceab8feec47c373c",
+    "url": "/static/media/about-bg2.e21119b2.jpg"
+  },
+  {
+    "revision": "5ae428f2ecac24d9328c3bf4772ebd02",
+    "url": "/static/media/team.5ae428f2.jpeg"
+  },
+  {
+    "revision": "43039c83afc6c034500e314af5bc971b",
+    "url": "/static/media/maggie.43039c83.jpg"
+  },
+  {
+    "revision": "ebd9a8a11a85d305e68d1a306f0afb26",
+    "url": "/static/media/ta.ebd9a8a1.jpg"
+  },
+  {
+    "revision": "c92c79b438882b7ad5564d98693fe30b",
+    "url": "/static/media/diego.c92c79b4.jpg"
+  },
+  {
+    "revision": "ef4aa3286d3a86d6797d6979c0088c56",
+    "url": "/static/media/julia.ef4aa328.jpg"
+  },
+  {
+    "revision": "7c6d22498663aed3136aea5f27b8830f",
+    "url": "/static/media/andra.7c6d2249.jpg"
+  },
+  {
+    "revision": "83533b9ee229115f671a628dfd7b4c2d",
+    "url": "/index.html"
+  }
+];
