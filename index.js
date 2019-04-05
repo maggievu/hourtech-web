b762d5635271ef59198cf9bf8c5041c64b5994c2
maggievu <maggievu91@gmail.com>
2019-04-04 18:55:33 -0700

    update cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors());
+// app.use(cors());
 
 // app.use(express.static(`${__dirname}/../client/build`))
 
@@ -21,11 +21,12 @@ const path = require('path')
 	});
 // }
 
-app.listen(process.env.PORT, '0.0.0.0', () => console.log(`Listening on port ${app.url}`))
+app.listen(port, () => console.log(`Listening on port ${port}`))
+// app.listen(process.env.PORT, '0.0.0.0', () => console.log(`Listening on port ${app.url}`))
 
 const nodemailer = require('nodemailer')
 
-app.post('/api/form', (req, res) => {
+app.post('/form', (req, res) => {
 	nodemailer.createTestAccount((err, account) => {
 		const htmlEmail = `
         <div>
