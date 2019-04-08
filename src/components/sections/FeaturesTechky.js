54845e7eb95904bc17f3436e2fb5216dc246812b
maggievu <maggievu91@gmail.com>
2019-04-07 22:19:34 -0700

    add to features techkies

--- src/components/sections/FeaturesTechky.js
+++ src/components/sections/FeaturesTechky.js
@@ -2,9 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import { Grid } from '@material-ui/core'
-import portfolio from '../../assets/img/portfolio.png'
+import profile from '../../assets/img/profile.png'
+import activity from '../../assets/img/activity.png'
 import messageBoard from '../../assets/img/message-board.png'
-// import activity from '../../assets/img/activity.png'
 
 const styles = {
   root: {
@@ -16,14 +16,14 @@ const styles = {
 const FeaturesTechky = (props) => (
   <Grid container spacing={40} className={props.classes.root}>
     <Grid item xs={12} md={4}>
-      <img width={200} src={portfolio} alt='Features for Techkies' />
+      <img width={200} src={profile} alt='Features for Techkies' />
       <h3>Pre-vetted</h3>
       <p>Send us your previous works to become certified</p>
     </Grid>
     <Grid item xs={12} md={4}>
-      <img width={200} src={portfolio} alt='Features for Techkies' />
-      <h3>Rating</h3>
-      <p>Rate and review the client that you worked for</p>
+      <img width={200} src={activity} alt='Features for Techkies' />
+      <h3>History</h3>
+      <p>Save favorite jobs. Review past and current jobs</p>
     </Grid>
     <Grid item xs={12} md={4}>
       <img width={200} src={messageBoard} alt='Features for Techkies' />
